206835facfc8ed156ce5bdfced7932bf79b80a05
nschimka <nschimka@yahoo.com>
2025-04-28 21:37:29 -0500

    start looking into claude, chatgpt, deepseek

--- README.md
+++ README.md
@@ -7,8 +7,27 @@ To run `collect_responses`, you'll need to configure several environmental varia
 ```
 GEMINI_API_KEY="" # https://aistudio.google.com/app/apikey
 VERTEX_AI_PROJECT_ID="" # https://cloud.google.com/vertex-ai?hl=en
+AZURE_OPENAI_API_KEY=""
+AZURE_OPENAI_ENDPOINT=""
+DEEPSEEK_API_KEY=""
 ```
 
 ### Google setup
 
-Claude and Google Translate are accessed via Google. Download the gcloud CLI (https://cloud.google.com/sdk/docs/install) and then run `gcloud init`, `gcloud auth application-default login` to set up your credentials.
+Claude and Google Translate are accessed via Google. 
+
+1) Download the gcloud CLI (https://cloud.google.com/sdk/docs/install)
+2) Run `gcloud init`
+3) Run `gcloud auth application-default login` to set up your credentials. This will save a credentials file in a set location based on your OS (https://cloud.google.com/docs/authentication/application-default-credentials#personal)
+4) Get your project ID and enable the Vertex AI API (https://console.cloud.google.com/apis/library/aiplatform.googleapis.com) and the Claude 3.7 Sonnet API (https://console.cloud.google.com/vertex-ai/publishers/anthropic/model-garden/claude-3-7-sonnet)
+
+### Microsoft Azure setup
+
+ChatGPT is accessed via Azure.
+
+1) Within the Azure online console, create an OpenAI account
+2) Once your service deploys, navigate to it and click "Keys and Endpoint" under the "Resource Management" menu item. You can use either key 1 or key 2 as your `AZURE_OPENAI_API_KEY`; your `AZURE_OPENAI_ENDPOINT` is also on this page.
+
+### DeepSeek setup
+
+Create an API key: https://platform.deepseek.com/api_keys
--- collect_responses.py
+++ collect_responses.py
@@ -3,22 +3,24 @@ from dotenv import load_dotenv
 from google import genai
 from anthropic import AnthropicVertex
 from google.cloud import translate_v2
+from openai import AzureOpenAI
+from openai import OpenAI
 
 load_dotenv()
 
 gemini_key = os.getenv("GEMINI_API_KEY")
 
 # Gemini test
-client = genai.Client(api_key=gemini_key)
+""" client = genai.Client(api_key=gemini_key)
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
 )
-print(response.text)
+print(response.text) """
 
-
-# Claude test via Vertex AI
 """
-https://docs.anthropic.com/en/api/claude-on-vertex-ai
+# Claude test via Vertex AI
+# https://docs.anthropic.com/en/api/claude-on-vertex-ai
+# This can't be used with the free $300 trial. Maybe the $50 student credits?
 LOCATION="us-east5"
 client = AnthropicVertex(region=LOCATION, project_id=os.getenv("VERTEX_AI_PROJECT_ID"))
 
@@ -38,5 +40,37 @@ print(message.model_dump_json(indent=2))
 # Google translate test
 
 # Deepseek
+# Deepseek directly costs money... try OpenRouter. 50 free requests a day? https://openrouter.ai/deepseek/deepseek-chat-v3/api
+client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
 
 # ChatGPT via Azure
+"""
+client = AzureOpenAI(
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+  api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
+  api_version="2024-02-01"
+)
+
+response = client.chat.completions.create(
+    model="gpt-4o", # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure AI services support this too?"}
+    ]
+)
+
+print(response.choices[0].message.content)
+"""
