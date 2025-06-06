bca46788531c24f18f5922257dd14f77e803d9e1
nschimka <nschimka@yahoo.com>
2025-06-05 22:05:49 -0500

    try out some more tokenizers

--- evaluation.py
+++ evaluation.py
@@ -29,7 +29,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from evaluate import load
 from sacrebleu.tokenizers.tokenizer_spm import Flores101Tokenizer
-from sacrebleu.tokenizers.tokenizer_13a import Tokenizer13a
+from sacrebleu.tokenizers.tokenizer_intl import TokenizerV14International
 from sacrebleu.tokenizers.tokenizer_zh import TokenizerZh
 
 import pandas as pd
@@ -45,10 +45,10 @@ class EvaluationTokenizer:
         match language:
             case "chinese_traditional":
                 tokenizer = TokenizerZh()
-            case "arabic":
+            case "arabic", "vietnamese":
                 tokenizer = Flores101Tokenizer()
-            case _:
-                tokenizer = Tokenizer13a()
+            case _: # spanish, haitian creole
+                tokenizer = TokenizerV14International()
         return tokenizer
 
     def __init__(self, language):
