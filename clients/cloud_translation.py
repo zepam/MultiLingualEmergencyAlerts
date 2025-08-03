356ff512b79bf77b43c3db9f6c53a83757b61424
Jen Wilson <12726758+zepam@users.noreply.github.com>
2025-08-02 21:30:35 -0700

    rename file

--- clients/cloud_translation.py
+++ clients/cloud_translation.py
@@ -5,11 +5,15 @@ from clients.client import Client
 class GoogleCloudTranslationClient(Client):
     def __init__(self, logger, key="unused"):
         super().__init__(key, logger)
+        # pass the project ID into the constructor
+        # This is required for the Google Cloud Translation API to work properly
+        # or you can set the GOOGLE_CLOUD_PROJECT environment variable
+        self.project_id = "multilingual-alerts"
 
     def chat(self, prompt_file, disaster, language, sending_agency=None, location=None, time=None, url=None):
         prompt = self.gather_prompt(prompt_file=prompt_file, disaster=disaster, language=language,
                                     sending_agency=sending_agency, location=location, time=time, url=url)
-        translate_client = translate.Client()
+        translate_client = translate.Client(project=self.project_id)
 
         language_code = self.translation_map()[language]
         result = translate_client.translate(prompt, target_language=language_code)
