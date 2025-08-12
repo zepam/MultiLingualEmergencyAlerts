0547ba0ea92c4118a144c82f68e122d4cdff0691
Jen Wilson <12726758+zepam@users.noreply.github.com>
2025-08-12 00:17:13 -0700

    update google translate client

--- clients/cloud_translation.py
+++ clients/cloud_translation.py
@@ -8,12 +8,14 @@ class GoogleCloudTranslationClient(Client):
         # pass the project ID into the constructor
         # This is required for the Google Cloud Translation API to work properly
         # or you can set the GOOGLE_CLOUD_PROJECT environment variable
-        self.project_id = "multilingual-alerts"
+        self.project_id = "multilingual-alerts-460703"
 
     def chat(self, prompt_file, disaster, language, sending_agency=None, location=None, time=None, url=None):
         prompt = self.gather_prompt(prompt_file=prompt_file, disaster=disaster, language=language,
                                     sending_agency=sending_agency, location=location, time=time, url=url)
-        translate_client = translate.TranslationServiceClient()
+        translate_client = translate.TranslationServiceClient(
+            client_options={"quota_project_id": self.project_id}
+        )
 
         language_code = self.translation_map()[language]
 
