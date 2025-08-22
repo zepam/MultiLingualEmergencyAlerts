e461f0e16ad0b458acafe1d96123b4e1a32908a5
Jen Wilson <12726758+zepam@users.noreply.github.com>
2025-08-22 15:46:11 -0700

    comment out languages without gold standards

--- clients/translation_map.py
+++ clients/translation_map.py
@@ -1,7 +1,6 @@
 # Shared language name to code mapping for all translation clients
 
 TRANSLATION_MAP = {
-    "English": "en-US", 
     "Spanish": "es",
     "Arabic": "ar",
     "Haitian Creole": "ht", 
@@ -10,15 +9,15 @@ TRANSLATION_MAP = {
     "Chinese (Simplified)": "zh",
     "Russian": "ru",        # 5000+ person language in WA state 2024
     "Ukrainian": "uk",      # 5000+ person language in WA state 2024
-    "Somali": "so",        # 5000+ person language in WA state 2024
-    "Dari": "prs",          # 5000+ person language in WA state 2024. sometimes also 'fa' for Farsi
-    "Marshallese": "mh",    # 5000+ person language in WA state 2024
-    "Korean": "ko",         # 5000+ person language in WA state 2024
-    "Punjabi": "pa",       # 5000+ person language in WA state 2024
-    "Hindi": "hi",         # 5000+ person language in WA state 2024
-    "Telegu": "te",       # 5000+ person language in WA state 2024
-    "Tagalog": "tl",       # 5000+ person language in WA state 2024
-    "Amharic": "am",       # 5000+ person language in WA state 2024
-    "Chuukese": "chk",     # 5000+ person language in WA state 2024
-    "Cambodian": "km",      # 5000+ person language in WA state 2024
+    # "Somali": "so",        # 5000+ person language in WA state 2024
+    # "Dari": "prs",          # 5000+ person language in WA state 2024. sometimes also 'fa' for Farsi
+    # "Marshallese": "mh",    # 5000+ person language in WA state 2024
+    # "Korean": "ko",         # 5000+ person language in WA state 2024
+    # "Punjabi": "pa",       # 5000+ person language in WA state 2024
+    # "Hindi": "hi",         # 5000+ person language in WA state 2024
+    # "Telegu": "te",       # 5000+ person language in WA state 2024
+    # "Tagalog": "tl",       # 5000+ person language in WA state 2024
+    # "Amharic": "am",       # 5000+ person language in WA state 2024
+    # "Chuukese": "chk",     # 5000+ person language in WA state 2024
+    # "Cambodian": "km",      # 5000+ person language in WA state 2024
 }
