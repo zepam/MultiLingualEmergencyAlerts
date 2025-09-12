ce1d26e3290cf662dd3e451dde777704dcf03859
Jen Wilson <12726758+zepam@users.noreply.github.com>
2025-09-12 11:42:46 -0700

    add somali, marshallese, punjabi, telegu, amharic, cambodian, japanese, tigrinya, rudu, samoan

--- clients/translation_map.py
+++ clients/translation_map.py
@@ -1,5 +1,6 @@
 # Shared language name to code mapping for all translation clients
-
+# Languages sourced from:
+# https://capaa.wa.gov/resources/languages/washington-state-language-access-plan
 TRANSLATION_MAP = {
     "Spanish": "es",
     "Arabic": "ar",
@@ -9,23 +10,23 @@ TRANSLATION_MAP = {
     "Chinese (Simplified)": "zh",
     "Russian": "ru",        # 5000+ person language in WA state 2024
     "Ukrainian": "uk",      # 5000+ person language in WA state 2024
-    # "Somali": "so",        # 5000+ person language in WA state 2024
+    "Somali": "so",        # 5000+ person language in WA state 2024
     # "Dari": "prs",          # 5000+ person language in WA state 2024. sometimes also 'fa' for Farsi
-    # "Marshallese": "mh",    # 5000+ person language in WA state 2024
+    "Marshallese": "mh",    # 5000+ person language in WA state 2024
     "Korean": "ko",         # 5000+ person language in WA state 2024
-    # "Punjabi": "pa",       # 5000+ person language in WA state 2024
+    "Punjabi": "pa",       # 5000+ person language in WA state 2024
     "Hindi": "hi",         # 5000+ person language in WA state 2024
-    # "Telegu": "te",       # 5000+ person language in WA state 2024
+    "Telegu": "te",       # 5000+ person language in WA state 2024
     "Tagalog": "tl",       # 5000+ person language in WA state 2024
-    # "Amharic": "am",       # 5000+ person language in WA state 2024
+    "Amharic": "am",       # 5000+ person language in WA state 2024
     # ? "Chuukese": "chk",     # 5000+ person language in WA state 2024
-    # "Cambodian": "km",      # 5000+ person language in WA state 2024
+    "Cambodian": "km",      # 5000+ person language in WA state 2024
     # "Pashto": "ps",         # 5000+ person language in WA state 2024
     # "Tamil": "ta",         # 5000+ person language in WA state 2024
-    # "Japanese": "ja",      # 5000+ person language in WA state 2024
+    "Japanese": "ja",      # 5000+ person language in WA state 2024
     "Portuguese": "pt",    # 5000+ person language in WA state 2024
-    # "Tigrinya": "ti",      # 5000+ person language in WA state 2024
-    # "Urdu": "ur",          # 5000+ person language in WA state 2024
+    "Tigrinya": "ti",      # 5000+ person language in WA state 2024
+    "Urdu": "ur",          # 5000+ person language in WA state 2024
     "French": "fr",       # 5000+ person language in WA state 2024
     # "Oromo": "om",          # 5000+ person language in WA state 2024
     # "Farsi": "fa",          # 5000+ person language in WA state 2024
@@ -33,6 +34,19 @@ TRANSLATION_MAP = {
     # "Swahili": "sw",       # 5000+ person language in WA state 2024
     # "Hebrew": "he",        # 5000+ person language in WA state 2024
     # "Marathi": "mr",      # 5000+ person language in WA state 2024
-    # "Samoan": "sm",       # 5000+ person language in WA state 2024
+    "Samoan": "sm",       # 5000+ person language in WA state 2024
     # "Malayalam": "ml",    # 5000+ person language in WA state 2024
+    # CHECK THE LANGUAGE CODES BELOW
+    # "Lao": "lo",          # 5000+ person language in WA state 2024
+    # "Romanian": "ro",     # 5000+ person language in WA state 2024
+    # "Nepali": "ne",       # 5000+ person language in WA state 2024
+    # "Hmong": "hmn",      # 5000+ person language in WA state 2024
+    # "Mixteco": "mix",     # 5000+ person language in WA state 2024
+    # "German": "de",       # 5000+ person language in WA state 2024
+    # "Filipino": "fil",    # 5000+ person language in WA state 2024
+    # "Burmese": "my",      # 5000+ person language in WA state 2024
+    # "Thai": "th",        # 5000+ person language in WA state 2024"
+    # "Oromo": "om",          # 5000+ person language in WA state 2024
+    # "Karen": "karen",     # 5000+ person language in WA state 2024
+
 }
