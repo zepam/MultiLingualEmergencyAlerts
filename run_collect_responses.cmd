1dc15e7abffeefa901b817cc3dadb2bdd41ce4e1
Jen Wilson <12726758+zepam@users.noreply.github.com>
2026-04-10 13:14:43 -0700

    add run_collect_responses.cmd condor script

---
+++ run_collect_responses.cmd
@@ -0,0 +1,13 @@
+executable = /usr/bin/python3
+arguments = collect_responses.py
+
+getenv = true
+
+output = collect_responses.out
+error = collect_responses.err
+log = collect_responses.log
+
+transfer_executable = false
+request_memory = 2*1024
+
+queue
